58a5c201841e90c1d7641ad9abb73b6ff16c4aef
simerpreetjasssal <s.jassal599@mybvc.ca>
2020-06-30 17:16:52 -0600

    Initial Commit

---
+++ Notes Clone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Notes Clone
+//
+//  Created by Simerpreet singh Jassal on 2020-06-30.
+//  Copyright © 2020 Simerpreet singh Jassal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
